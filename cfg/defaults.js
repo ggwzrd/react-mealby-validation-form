652c976d4e080ce823c9c0a111565bdfae39ed93
Raffaele <izziaraffaele@gmail.com>
2016-05-12 08:24:57 +0200

    Some changes to the structure

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -48,5 +48,19 @@ module.exports = {
   getDefaultModules: getDefaultModules,
   postcss: function () {
     return [];
+  },
+  stats: {
+    hash: false,
+    timings: false,
+    chunks: false,
+    chunkModules: false,
+    modules: false,
+    children: true,
+    version: true,
+    cached: false,
+    cachedAssets: false,
+    reasons: false,
+    source: false,
+    errorDetails: false
   }
 };
